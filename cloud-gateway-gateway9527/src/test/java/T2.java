7f0c761fea9626f8fd83b45955fac1cd4a78a416
jfyoudianshuai <1579262144@qq.com>
2021-08-13 11:03:04 +0800

    添加 gateway 网关

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.time.ZonedDateTime;
+
+public class T2 {
+
+    @Test
+    public void test(){
+        ZonedDateTime z=ZonedDateTime.now();
+        System.out.println(z);
+    }
+
+}
